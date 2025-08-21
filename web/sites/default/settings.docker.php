131ccb31615a063fe8168a2979c39d6be925862f
Shibin Das <shibinkidd@gmail.com>
2025-08-21 19:14:11 +0200

    Add settings.php

---
+++ web/sites/default/settings.docker.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "db";
+$port = 3306;
+$driver = "mysql";
+
+$databases['default']['default']['database'] = "db";
+$databases['default']['default']['username'] = "db";
+$databases['default']['default']['password'] = "db";
+$databases['default']['default']['host'] = $host;
+$databases['default']['default']['port'] = $port;
+$databases['default']['default']['driver'] = $driver;
